9ce46d959bd2fe9e5403c0cd62a5faecdfb0fab0
awais-urrehman <awais.jarral@issm.ai>
2025-08-24 02:32:45 +0500

    added: gradient

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 import { Header } from './header'
 import { Footer } from './footer'
 import { ThemeProvider } from 'next-themes'
+import { GlassBackground } from '@/components/ui/glass-background'
 
 export const viewport: Viewport = {
   width: 'device-width',
@@ -44,6 +45,7 @@ export default function RootLayout({
           defaultTheme="system"
         >
           <div className="flex min-h-screen w-full flex-col font-[family-name:var(--font-inter-tight)]">
+            <GlassBackground />
             <div className="relative mx-auto w-full max-w-screen-sm flex-1 px-4 pt-20">
               <Header />
               {children}
